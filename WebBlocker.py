c2e6980e2c9f3a3cd61fb20fa12a190f33f1176b
Gabriel Bocchini <gabrielbocchini@gmail.com>
2019-01-08 12:23:12 -0200

    Add files via upload

--- WebBlocker.py
+++ WebBlocker.py
@@ -2,17 +2,24 @@ import sys
 import time
 from progressbar import ProgressBar, Percentage, Bar
 
+# estabelecendo objetos para o arquivo hosts, redirect address e lista inicial de sites bloqueáveis
+
 hosts_path = "C:/Windows/System32/drivers/etc/hosts"
 
 redirect = "127.0.0.1"
 
 web_list=['facebook.com', 'www.facebook.com', 'youtube.com', 'www.youtube.com', 'web.whatsapp.com', 'xvideos.com', 'www.xvideos.com']
 
+#abertura e leitura do arquivo hosts
 file = open(hosts_path, 'r+')
 content = file.read()
 
+#inicio da interação com o user
+
 print('\n\n'"Este programa irá bloquear por padrão os seguintes sites: "+str(web_list[0:]),'\n')
 
+
+#adicionar mais sites a lista
 pergunta = input('\n'"Gostaría de bloquear outro website? (y/n): ")
 
 if pergunta.lower() == "y":
@@ -22,6 +29,7 @@ if pergunta.lower() == "y":
 else:
     pass
 
+#remover elementos da lista
 pergunta2 = input('\n'"Gostaría de excluir algum website da lista? (y/n): "'\n')
 if pergunta2.lower() == 'y':
     opcao2 = input('\n'"Qual? (1 website, correspondencia exata): "'\n')
@@ -32,13 +40,14 @@ else:
 
 print("Bloqueando: \n"+str(web_list[0:])+'\n')
 
+#escrevendo sites adicionais na lista de sites a serem bloquados
 for website in web_list:
     if website in content:
         pass
     else:
         file.write(redirect+" "+website+"\n")
 
-
+#loading do processo e finalização do script
 progress = ProgressBar(widgets=[Percentage(), Bar()], maxval = 500000).start()
 progvar = 0
 
